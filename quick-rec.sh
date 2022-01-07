d0637711c4e7deb66cc3ab9332e68f2602d08171
Anju Jose <94441731+AnjuJose-Kunju@users.noreply.github.com>
2022-01-07 11:20:06 -0500

    Update quick-rec.sh

--- quick-rec.sh
+++ quick-rec.sh
@@ -27,3 +27,28 @@ rm $domain/$domain.txt
 }
 
 emailgrab
+
+subdomain enumeration
+
+#!/bin/bash
+echo "finding subdomains...."
+read -p "enter domain name:" domain
+echo "Finding subdomains using assetfinder is time consuming please wait"
+if [[ $domain != "" ]]; then
+	#echo "collecting the subdomains using  assetfinder subs-only it may take some time please wait"
+	touch subdomain_subs.txt subdomain_alive.txt subdomain_sorted.txt
+	assetfinder --subs-only $domain > subdomain_subs.txt
+	#echo "collecting the subdomains using  httprobe it may take some time please wait"
+	cat subdomain_subs.txt | httprobe > subdomain_alive.txt
+ 	cat subdomain_alive.txt | sort -u > subdomain_sorted.txt
+    cat subdomain_sorted.txt
+ 	count=$( wc -l < subdomain_sorted.txt )
+ 	echo "The total number of subdomains of $domain is $count"
+ else
+ 	echo "please enter a domain name"
+ fi
+ echo "--------------------------------------------------------"
+ rm subdomain_subs.txt
+ rm subdomain_alive.txt
+
+subdomain enumeration
