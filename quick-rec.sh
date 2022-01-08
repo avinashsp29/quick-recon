2a8af989a8de8633c257c578e0f9e9ea37acccad
Anju Jose <94441731+AnjuJose-Kunju@users.noreply.github.com>
2022-01-08 00:40:54 -0500

    Update quick-rec.sh

--- quick-rec.sh
+++ quick-rec.sh
@@ -25,30 +25,24 @@ fi
 
 rm $domain/$domain.txt
 }
-
-emailgrab
-
-subdomain enumeration
-
-#!/bin/bash
-echo "finding subdomains...."
-read -p "enter domain name:" domain
-echo "Finding subdomains using assetfinder is time consuming please wait"
+subdomain_finder()
+{
+echo -e "\n${bold}Finding subdomains using assetfinder is time consuming please wait...${end}\n"
 if [[ $domain != "" ]]; then
-	#echo "collecting the subdomains using  assetfinder subs-only it may take some time please wait"
-	touch subdomain_subs.txt subdomain_alive.txt subdomain_sorted.txt
+	touch subdomain_subs.txt subdomain_alive.txt 
 	assetfinder --subs-only $domain > subdomain_subs.txt
-	#echo "collecting the subdomains using  httprobe it may take some time please wait"
 	cat subdomain_subs.txt | httprobe > subdomain_alive.txt
- 	cat subdomain_alive.txt | sort -u > subdomain_sorted.txt
-    cat subdomain_sorted.txt
- 	count=$( wc -l < subdomain_sorted.txt )
- 	echo "The total number of subdomains of $domain is $count"
+ 	cat subdomain_alive.txt | sort -u > $domain/subdomain_sorted.txt
+        cat $domain/subdomain_sorted.txt
+ 	count=$( wc -l < $domain/subdomain_sorted.txt )
+ 	echo "\n${bold}The total number of subdomains of $domain is $count...${end}\n"
  else
- 	echo "please enter a domain name"
+ 	echo "\n${bold}please enter a domain name${end}"
  fi
- echo "--------------------------------------------------------"
+ echo -e "\n${bold}${green}Subdomains stored in $domain/subdomain_sorted.txt${end}"
  rm subdomain_subs.txt
  rm subdomain_alive.txt
+}
+emailgrab
+subdomain_finder
 
-subdomain enumeration
