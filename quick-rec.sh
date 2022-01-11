063c0b2aa16a9477e00ebff4aa46c346055ee7a6
J. Ayyappan <94441580+J-Ayyappan@users.noreply.github.com>
2022-01-11 20:24:16 +0530

    Update quick-rec.sh

--- quick-rec.sh
+++ quick-rec.sh
@@ -10,26 +10,27 @@ echo "|-- |--| |--| _|_  |--| |,.' | |--- _|_  | |--- ----       "
 echo "|   |__| |__|  |__ |--| |    | |  |  |__ | |  | |__|       "
 echo "                   |                            .__| ....! "
 echo -e "\033[1m\033[0\n"
-read -p "~enter domain name: " domain
+read -p "~Enter domain name: " domain
 echo -e "\033[1mSearching for domain...\033[0\n"
 mkdir $domain
-echo "~whois search results for $domain"
+
 whois_domain(){
- whois $domain > $domain/whois1.txt
- {
+echo "~whois search results for $domain"
+whois $domain > $domain/whois1.txt
+{
    grep -w "Domain Name" $domain/whois1.txt
    grep -w "Registrant" $domain/whois1.txt
    grep -w "Admin" $domain/whois1.txt
    grep -w "Registry" $domain/whois1.txt
    grep -w "Registrar" $domain/whois1.txt
- } > $domain/whois.txt
+} > $domain/whois.txt
 cat $domain/whois.txt
 echo -e "\n${bold}${green}whois results stored in $domain/whois.txt${end}"
 rm $domain/whois1.txt
 }
-touch $domain/email.txt
 
 emailgrab(){
+touch $domain/email.txt
 echo -e "\n${bold}Collecting email IDs. This may take some time...${end}\n"
 wget -qNr -l 1 "$domain" -O "$domain/$domain.txt"
 cat "$domain/$domain.txt" | grep -aoE "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" >> $domain/email.txt
@@ -42,8 +43,10 @@ else
 cat $domain/email.txt
 echo -e "\n${bold}${green}Email IDs stored in $domain/email.txt${end}"
 fi
+
 rm $domain/$domain.txt
 }
+
 subdomain_finder()
 {
 echo -e "\n${bold}Finding subdomains using assetfinder is time consuming please wait...${end}\n"
@@ -57,11 +60,11 @@ echo -e "\n${bold}${green}Subdomains stored in $domain/subdomain_sorted.txt${end
 rm $domain/subdomain_subs.txt
 rm $domain/subdomain_alive.txt
 }
+
 if [[ $domain != "" ]]; then
 whois_domain
 emailgrab
 subdomain_finder
- else
-  echo "\n${bold}please enter a domain name${end}"
- fi
-
+else
+echo "\n${bold}Please enter a valid domain name${end}"
+fi
